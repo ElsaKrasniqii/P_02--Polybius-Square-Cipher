0986c9693561c10ad4d731e12ca3a62cd5fa5f8e
elejerlija <elajerlija3@gmail.com>
2025-04-11 23:33:29 +0200

    Krijimi i struktures baze se main menu

--- .idea/src/PolybiusSquareCipher/PolybiusSquareCipher.java
+++
@@ -1,7 +0,0 @@
-package PolybiusSquareCipher;
-
-public class PolybiusSquareCipher {
-    public static void main(String[] args){
-
-    }
-}
---
+++ src/PolybiusSquareCipher/PolybiusSquareCipher.java
@@ -0,0 +1,40 @@
+package PolybiusSquareCipher;
+
+import java.util.Scanner;
+
+public class PolybiusSquareCipher {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("Zgjedh një proces: ");
+            System.out.println("1. Enkriptim");
+            System.out.println("2. Dekriptim");
+            System.out.println("3. Shfaq Shembujt (Enkriptim dhe Dekriptim)");
+            System.out.println("0. Dalje");
+            int choice = scanner.nextInt();
+            scanner.nextLine();
+
+            if (choice == 1) {
+
+            } else if (choice == 2) {
+
+            } else if (choice == 3) {
+
+            } else if (choice == 0) {
+                System.out.println("Dalim...");
+                break;
+            } else {
+                System.out.println("Zgjedhje e pavlefshme. Provoni përsëri.");
+            }
+
+            System.out.println("Deshironi të vazhdoni? (Po/Jo):");
+            String continueChoice = scanner.nextLine();
+            if (!continueChoice.equalsIgnoreCase("Po")) {
+                break;
+            }
+        }
+
+        scanner.close();
+    }
+}
