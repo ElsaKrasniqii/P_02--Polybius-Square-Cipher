82f1f1ac059419f288b485eb3476be4422095191
elejerlija <elajerlija3@gmail.com>
2025-04-12 22:07:27 +0200

    Thirrja e klasave per enkriptim dhe dekriptim

--- src/PolybiusSquareCipher/PolybiusSquareCipher.java
+++ src/PolybiusSquareCipher/PolybiusSquareCipher.java
@@ -2,7 +2,7 @@ package PolybiusSquareCipher;
 
 import java.util.Scanner;
 
-public class 2PolybiusSquareCipher {
+public class PolybiusSquareCipher {
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
 
@@ -16,9 +16,15 @@ public class 2PolybiusSquareCipher {
             scanner.nextLine();
 
             if (choice == 1) {
-
+                System.out.println("Shkruaj fjalën për enkriptim:");
+                String text = scanner.nextLine();
+                String encryptedText = PolybiusSquareEncrypt.encrypt(text);
+                System.out.println("Teksti i enkriptuar është: " + encryptedText);
             } else if (choice == 2) {
-
+                System.out.println("Shkruaj fjalën për dekriptim:");
+                String cipherText = scanner.nextLine();
+                String decryptedText = PolybiusSquareDecrypt.decrypt(cipherText);
+                System.out.println("Teksti i dekriptuar është: " + decryptedText);
             } else if (choice == 3) {
 
             } else if (choice == 0) {
